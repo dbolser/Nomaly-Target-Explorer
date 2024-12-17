9c4c6537c554ffe02259a8ff1516236a716d1796
Chang Lu <luchang.thu@outlook.com>
2024-12-17 12:05:09 +0000

    diseases for georgina to look at

--- blueprints/page1.py
+++ blueprints/page1.py
@@ -1,28 +1,18 @@
-from flask import Blueprint, render_template
+from flask import render_template, request, jsonify, Blueprint
+from flask import session, redirect, url_for
+from db import get_db_connection
 import pandas as pd
 
+# ----------------------------------------------------- #
+# Phecode Blueprint
+# ----------------------------------------------------- #
+
 # Create the blueprint
-page1_bp = Blueprint('page1', __name__)
+# page1_bp = Blueprint('page1', __name__)
+disease_select = Blueprint('disease', __name__, template_folder='../templates')
 
-# Sample data for each table
-datadir = '/data/general/UKBB/Phenotypes/'
-
-individual_phenotypes_data = pd.read_csv(datadir + 'individual_phenotypes.tsv', sep='\t')[[
-    'Sex', 'suppop', 'icd10_any', 'phecodes'
-]].rename(columns={'Sex':'sex', 'icd10_any': 'icd10', 'phecodes': 'phecode', 'suppop': 'population'}).to_dict(orient='records')
-
-phecode_data = pd.read_csv(datadir + 'phecodes.tsv', sep='\t', dtype={'phecode': str})[[
-    'phecode', 'description', 'sex', 'group', 'phecode_exclude_range', 'cases', 'exclude'
-]].rename(columns={'exclude': 'excluded', 'phecode_exclude_range': 'phecode_exclude'}).to_dict(orient='records')
-
-icd10_coding_data = pd.read_csv(datadir + "createUKBphenome/data/coding19.tsv", sep='\t')[[
-    'coding', 'meaning'
-]].to_dict(orient='records')
 
 # Route for page1 with tables
-@page1_bp.route('/page1')
-def show_tables():
-    return render_template('page1.html', 
-                           individual_phenotypes=individual_phenotypes_data, 
-                           phecode=phecode_data, 
-                           icd10_coding=icd10_coding_data)
+@disease_select.route('/page1')
+def render_page1():
+    return render_template('page1.html')
