5c25a2fb96fee83db47a093c96aaf2a27ada6ed2
Dan Bolser <dan.bolser@outsee.co.uk>
2024-12-18 14:03:53 +0000

    Adding additional routes with different custom lists

--- blueprints/page1.py
+++ blueprints/page1.py
@@ -3,7 +3,6 @@ from flask import render_template, Blueprint
 disease_select = Blueprint("disease", __name__, template_folder="../templates")
 
 
-# Route for page1 with tables
 @disease_select.route("/page1")
 def render_page1():
     search_categories = {
@@ -15,6 +14,39 @@ def render_page1():
         "Women's Health": [
             "Polycystic Ovarian",
             "Endometriosis",
+        ],
+    }
+    return render_template("page1.html", search_categories=search_categories)
+
+
+@disease_select.route("/page-diabetes")
+def render_diabetes():
+    search_categories = {
+        "Diabetes Primary Conditions": [
+            "Diabetes",
+            "Type 1 diabetes",
+            "Type 2 diabetes",
+        ],
+        "Diabetes Complications": [
+            "Retinopathy",
+            "Nephropathy",
+            "Neuropathy",
+        ],
+    }
+    return render_template("page1.html", search_categories=search_categories)
+
+
+@disease_select.route("/page-dementia")
+def render_dementia():
+    search_categories = {
+        "Dementia Cognitive Disorders": [
+            "Dementia",
+            "Alzheimer",
+            "Cognitive decline",
+        ],
+        "Dementia Related Conditions": [
+            "Depression",
+            "Anxiety",
         ],
     }
     return render_template("page1.html", search_categories=search_categories)
