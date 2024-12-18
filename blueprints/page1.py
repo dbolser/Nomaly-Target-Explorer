a375fd46d19a2996c7f65890a21d9db441110842
Dan Bolser <dan.bolser@outsee.co.uk>
2024-12-18 12:01:04 +0000

    Adding a 'category' level in the list

--- blueprints/page1.py
+++ blueprints/page1.py
@@ -15,11 +15,16 @@ disease_select = Blueprint("disease", __name__, template_folder="../templates")
 # Route for page1 with tables
 @disease_select.route("/page1")
 def render_page1():
-    search_terms = [
-        "Hidradenitis",
-        "Rosacea",
-        "Ehlers",
-        "Polycystic Ovarian",
-        "Endometriosis",
-    ]
-    return render_template("page1.html", search_terms=search_terms)
+    search_categories = {
+        "Skin Conditions": [
+            "Hidradenitis",
+            "Rosacea",
+            "Ehlers",
+        ],
+        "Women's Health": [
+            "Polycystic Ovarian",
+            "Endometriosis",
+            "Ehlers",
+        ],
+    }
+    return render_template("page1.html", search_categories=search_categories)
