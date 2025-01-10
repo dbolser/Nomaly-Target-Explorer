3baf961169344937123a0247f752ca75ae5c2e2d
Dan Bolser <dan.bolser@outsee.co.uk>
2025-01-10 11:27:43 +0000

    This is a trick to 'force' connection via TCP rather than socket

--- db.py
+++ db.py
@@ -18,6 +18,7 @@ def get_db_connection() -> MySQLConnectionAbstract:
         with app_context():
             conn = mysql.connector.connect(
                 host=current_app.config["MYSQL_HOST"],
+                port=current_app.config["MYSQL_PORT"],
                 user=current_app.config["MYSQL_USER"],
                 password=current_app.config["MYSQL_PASSWORD"],
                 database=current_app.config["MYSQL_DB"],
