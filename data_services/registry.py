93c23134ae6730b1920ddcd9bcd8b79c0e43f0aa
Dan Bolser <dan.bolser@outsee.co.uk>
2025-02-14 20:09:24 +0000

    Helping to refactor..

--- data_services/registry.py
+++ data_services/registry.py
@@ -34,3 +34,16 @@ class ServiceRegistry:
         # Please don't ask
         self.stats_v2 = StatsService(app.config.get("STATS_H5_V2"))
         self.nomaly_score_v2 = NomalyScoreService(app.config.get("NOMALY_SCORES_H5_V2"))
+
+    def init_from_config(self, config):
+        self.genotype = GenotypeService(config.GENOTYPES_H5)
+        self.phenotype = PhenotypeService(config.PHENOTYPES_H5)
+        self.stats = StatsService(config.STATS_H5)
+        self.nomaly_score = NomalyScoreService(config.NOMALY_SCORES_H5)
+
+    @classmethod
+    def from_config(cls, config):
+        registry = cls()
+        registry.init_from_config(config)
+
+        return registry
