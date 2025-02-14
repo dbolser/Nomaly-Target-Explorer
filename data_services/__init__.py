c489383f5f282464958d9ba34c023dca36f73bc1
Dan Bolser <dan.bolser@outsee.co.uk>
2025-02-14 20:09:54 +0000

    Random junk

---
+++ data_services/__init__.py
@@ -0,0 +1,5 @@
+from .registry import ServiceRegistry
+from .genotype import GenotypeService
+from .phenotype import PhenotypeService
+
+__all__ = ["ServiceRegistry", "GenotypeService", "PhenotypeService"]
