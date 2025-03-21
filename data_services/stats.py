fb82b07d72b45a7484415f5d3cc0e68bc6a7edf8
Dan Bolser <dan.bolser@outsee.co.uk>
2025-03-21 14:06:44 +0000

    Enhance StatsService with new methods for retrieving term and phecode statistics. Updated get method to include default parameters. Refactored data retrieval logic in StatsHDF5 for improved clarity and error handling. Adjusted comments for consistency and clarity.

--- data_services/stats.py
+++ data_services/stats.py
@@ -1,9 +1,12 @@
 from pathlib import Path
-from typing import Optional, Dict, Tuple
+from typing import Dict, List, Optional, Tuple, Literal
 
 import h5py
 import numpy as np
 import pandas as pd
+
+from data_services.utils import deprecated
+
 
 class StatsRegistry:
     """Registry for managing multiple StatsService instances."""
@@ -12,7 +15,7 @@ class StatsRegistry:
         self.stats_selector = stats_selector
         self._services: Dict[Tuple[str, str], StatsService] = {}
 
-    def get(self, run_version: str, ancestry: str) -> "StatsService":
+    def get(self, run_version: str = "Run-v1", ancestry: str = "EUR") -> "StatsService":
         """Get a StatsService instance for the specified run version and ancestry.
 
         Args:
@@ -41,11 +44,61 @@ class StatsService:
     def __init__(self, hdf5_file: Path | str):
         self._hdf = StatsHDF5(hdf5_file)
 
-    # Expose the StatsHDF5 methods here
-    def get_stats_by_term_phecode(
-        self, term: str, phecode: str, statstype: Optional[str] = None
-    ) -> np.ndarray:
-        return self._hdf.get_stats_by_term_phecode(term, phecode, statstype)
+    def get_term_stats(
+        self,
+        term: str,
+        phecode: str | None = None,
+        phecodes: List[str] | None = None,
+        stats_types: List[str] | None = None,
+    ) -> pd.DataFrame:
+        """Get data for all terms for a specific phecode.
+
+        Args:
+            term: The term to get data for
+            phecode: The phecode to get data for OR
+            phecodes: One or more phecodes to include, or None for all
+            stats_types: One or more stats types to include, or None for all
+
+        Returns:
+            DataFrame for the term with phecodes as index, stats_types as columns
+        """
+        if phecode and phecodes:
+            raise ValueError("Don't be silly.")
+        phecodes = [phecode] if phecode is not None else phecodes
+        return self._hdf.get_data_slice(
+            t=[term],
+            p=phecodes,
+            s=stats_types,
+            invoker="get_term_stats",
+        )
+
+    def get_phecode_stats(
+        self,
+        phecode: str,
+        term: str | None = None,
+        terms: List[str] | None = None,
+        stats_types: List[str] | None = None,
+    ) -> pd.DataFrame:
+        """Get data for all phecodes for a specific term.
+
+        Args:
+            phecode: The phecode to get data for
+            term: The term to get data for
+            terms: One or more terms to include, or None for all
+            stats_types: One or more stats types to include, or None for all
+
+        Returns:
+            DataFrame with phecodes as index, stats_types as columns
+        """
+        if term and terms:
+            raise ValueError("Don't be silly.")
+        terms = [term] if term is not None else terms
+        return self._hdf.get_data_slice(
+            t=terms,
+            p=[phecode],
+            s=stats_types,
+            invoker="get_phecode_stats",
+        )
 
 
 class StatsHDF5:
@@ -54,141 +107,135 @@ class StatsHDF5:
     def __init__(self, hdf5_file):
         self.f = h5py.File(hdf5_file, "r")
 
-        # TODO: Fix the naming of the fields
         data = self.f["data"]
-        term = self.f["term"]
-        phecode = self.f["phecode"]
-        stats_type = self.f["stats_type"]
+        terms = self.f["term"]  # TODO: Fix the naming of the fields
+        phecodes = self.f["phecode"]  # TODO: Fix the naming of the fields
+        stats_types = self.f["stats_type"]  # TODO: Fix the naming of the fields
 
         # Jumping through hoops to fix the type checker...
         assert isinstance(data, h5py.Dataset)
-        assert isinstance(term, h5py.Dataset)
-        assert isinstance(phecode, h5py.Dataset)
-        assert isinstance(stats_type, h5py.Dataset)
+        assert isinstance(terms, h5py.Dataset)
+        assert isinstance(phecodes, h5py.Dataset)
+        assert isinstance(stats_types, h5py.Dataset)
 
         # Sanity checks
         # TODO: Move these to integration tests?
         try:
-            print(f"RUNNING SANITY CHECK ON {hdf5_file}! ONLY SEE THIS ONCE!")
-            assert data.shape[0] == term.shape[0]
-            assert data.shape[1] == phecode.shape[0]
-            assert data.shape[2] == stats_type.shape[0]
+            print(f"RUNNING SANITY CHECK ON {self.f.filename}! ONLY SEE THIS ONCE!")
+            assert data.shape[0] == terms.shape[0]
+            assert data.shape[1] == phecodes.shape[0]
+            assert data.shape[2] == stats_types.shape[0]
         except Exception as e:
             print(f"Error in sanity checks: {str(e)}")
             raise
 
         # Load the data matrix and index information as numpy arrays
         self.data: h5py.Dataset = data
-        self.term: np.ndarray = term[...].astype(str)
-        self.phecode: np.ndarray = phecode[...].astype(str)
-        self.stats_type: np.ndarray = stats_type[...].astype(str)
+        self.terms: np.ndarray = terms[...].astype(str)
+        self.phecodes: np.ndarray = phecodes[...].astype(str)
+        self.stats_types: np.ndarray = stats_types[...].astype(str)
 
-    # TODO: ALL THESE FUNCTIONS SHOULD BE ONE GENERAL FUNTION WITH 3 OPTIONAL ARGUMENTS
-    # TODO: 1. phecode (str|list), 2. term (str|list), 3. statstype (str|list)
-    # TODO: Return type should be consistent.
-
-    def get_stats_wip(
+    def get_data_slice(
         self,
-        phecode: None | str | list = None,
-        term: None | str | list = None,
-        statstype: None | str | list = None,
+        t: Optional[List[str]] = None,
+        p: Optional[List[str]] = None,
+        s: Optional[List[str]] = None,
+        invoker: Literal["get_term_stats", "get_phecode_stats"] | None = None,
     ) -> pd.DataFrame:
-        if isinstance(phecode, str):
-            phecode = [phecode]
-        if isinstance(term, str):
-            term = [term]
-        if isinstance(statstype, str):
-            statstype = [statstype]
+        """Get a slice of the data based on the provided filters.
 
-        if not phecode and not term:
-            raise ValueError(
-                "What the fuck are you doing? At least one of phecode or term must be provided!"
-            )
-        elif phecode and term:
-            if not len(phecode) == 1 or not len(term) == 1:
-                raise ValueError(
-                    "What the fuck are you doing? Either phecode or term must be a single value!"
-                )
+        Args:
+            terms: One or more terms to select, or None for all
+            phecodes: One or more phecodes to select, or None for all
+            stats_types: One or more stats types to select, or None for all
 
-        if phecode is None:
-            phecode_mask = np.ones(len(self.phecode), dtype=bool)
-        else:
-            phecode_mask = np.isin(self.phecode, phecode)
+        Returns:
+            DataFrame with the requested data slice
 
-        if term is None:
-            term_mask = np.ones(len(self.term), dtype=bool)
-        else:
-            term_mask = np.isin(self.term, term)
+        Raises:
+            ValueError: If neither terms nor phecodes are provided
+        """
 
-        if statstype is None:
-            statstype_mask = np.ones(len(self.stats_type), dtype=bool)
-        else:
-            statstype_mask = np.isin(self.stats_type, statstype)
+        # At least one of terms or phecodes must be provided
+        if t is None and p is None:
+            raise ValueError("At least one of terms or phecodes must be provided")
 
-        # NOTE: All data is stored as 'float' in the HDF, however, some columns
-        # are actually integers. TODO: This is fine, but we could convert to int
-        # here.
-        return pd.DataFrame(
-            self.data[term_mask][:, phecode_mask][:, :, statstype_mask],
-            index=self.term[term_mask],
-            columns=self.phecode[phecode_mask],
-            dtype=float,
+        # Create masks for each dimension
+        t_msk = (
+            np.ones(len(self.terms), dtype=bool)
+            if t is None
+            else np.isin(self.terms, t)
+        )
+        p_msk = (
+            np.ones(len(self.phecodes), dtype=bool)
+            if p is None
+            else np.isin(self.phecodes, p)
+        )
+        s_msk = (
+            np.ones(len(self.stats_types), dtype=bool)
+            if s is None
+            else np.isin(self.stats_types, s)
         )
 
-    def get_stats_by_phecode(
-        self, phecode: str, statstype: None | str | list = None
-    ) -> pd.DataFrame | np.ndarray:
-        if phecode not in self.phecode:
-            raise ValueError(f"Disease {phecode} not found in dataset")
+        # Get the filtered indices
+        t_idx = np.where(t_msk)[0]
+        p_idx = np.where(p_msk)[0]
+        s_idx = np.where(s_msk)[0]
 
-        mask_column = self.phecode == phecode
-        mask_column_indices = np.where(mask_column)[0][0]
-
-        if statstype is None:
-            # Return all statstypes for the disease
-            disease_data = self.data[:, mask_column_indices, :]
-            return pd.DataFrame(
-                disease_data, index=self.term.astype(str), columns=self.stats_type
+        if len(t_idx) == 0:
+            raise ValueError(
+                f"No data found for the provided terms in {self.f.filename}\n"
+                f"Terms provided: {t}\n"
+                f"Terms in dataset: {self.terms}\n"
+            )
+        if len(p_idx) == 0:
+            raise ValueError(
+                f"No data found for the provided phecodes in {self.f.filename}\n"
+                f"Phecodes provided: {p}\n"
+                f"Phecodes in dataset: {self.phecodes}\n"
+            )
+        if len(s_idx) == 0:
+            raise ValueError(
+                f"No data found for the provided stats types in {self.f.filename}\n"
+                f"Stats types provided: {s}\n"
+                f"Stats types in dataset: {self.stats_types}\n"
             )
 
-        # NOTE: The return types of these alternative calls are different!
-        elif isinstance(statstype, str):
-            # Return the data for the given statstype
-            mask_3rd_dim = self.stats_type == statstype
-            mask_3rd_dim_indices = np.where(mask_3rd_dim)[0][0]
-            return self.data[:, mask_column_indices, mask_3rd_dim_indices]
+        # TERM PHECODE / PHECODE TERM
+        if len(t_idx) == 1 and len(p_idx) == 1:
+            # Only s_idx is a 'fancy' index
+            results = self.data[t_idx[0], p_idx[0], s_idx]
+            columns = self.stats_types[s_idx]
+            if invoker is None:
+                index = None
+            elif invoker == "get_phecode_stats":
+                index = self.terms[t_idx]
+            elif invoker == "get_term_stats":
+                index = self.phecodes[p_idx]
 
-        elif isinstance(statstype, list):
-            # Return the data for the given statstypes
-            mask_3rd_dim = np.isin(self.stats_type, statstype)
-            mask_3rd_dim_indices = np.where(mask_3rd_dim)[0]
-            return self.data[:, mask_column_indices, mask_3rd_dim_indices]
+            return pd.DataFrame(results[np.newaxis, :], index=index, columns=columns)
 
-    # TODO: Perhaps allow phecode to also be a list like above?
-    # TODO: Think about making return type consistent.
-    def get_stats_by_term_phecode(
-        self, term: str, phecode: str, statstype: None | str | np.ndarray = None
-    ) -> dict[str, np.ndarray]:
-        if statstype is None:
-            statstype = self.stats_type
-        elif isinstance(statstype, str):
-            statstype = np.array([statstype])
-        elif isinstance(statstype, np.ndarray):
-            pass
+        # TERM PHECODES
+        if len(t_idx) == 1 and len(p_idx) > 1:
+            # Trying to do this with advanced indexing on 3 dimensions at once...
+            # Do two separate 'fancy indexes' instead.
+            results = self.data[t_idx[0], p_idx, :][:, s_idx]
 
-        term_mask = self.term == term
-        mask_row_indices = np.where(term_mask)[0][0]
+            index = self.stats_types[s_idx]
+            columns = self.phecodes[p_idx]
 
-        phecode_mask = self.phecode == phecode
-        mask_column_indices = np.where(phecode_mask)[0][0]
+            return pd.DataFrame(results.T, index=index, columns=columns)
 
-        statstype_mask = np.isin(self.stats_type, statstype)
-        statstype_mask_indices = np.where(statstype_mask)[0]
+        # TERMS PHECODE
+        if len(t_idx) > 1 and len(p_idx) == 1:
+            # Trying to do this with advanced indexing on 3 dimensions at once...
+            # Do two separate 'fancy indexes' instead.
+            results = self.data[t_idx, p_idx[0], :][:, s_idx]
 
-        # return a dictionary with the statstype as key and the value as the data
-        statsdict = {
-            self.stats_type[i]: self.data[mask_row_indices, mask_column_indices, i]
-            for i in statstype_mask_indices
-        }
+            index = self.terms[t_idx]
+            columns = self.stats_types[s_idx]
 
-        return statsdict
+            return pd.DataFrame(results, index=index, columns=columns)
+
+        # TODO: Just need to decide on format of the return value.
+        raise ValueError("Multiple terms and phecodes not currently supported")
