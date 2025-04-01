e0403a5e9cb7fd44a53c3132dd62134f93efc69a
Dan Bolser <dan.bolser@outsee.co.uk>
2025-04-01 23:35:29 +0100

    Tidied, and switched to calculating p-value on the pseudo counts!

--- blueprints/phewas.py
+++ blueprints/phewas.py
@@ -88,8 +88,8 @@ def run_phewas(
     # OK, this gets a bit slower, but, compared to the original crap, it's lightning fast...
 
     # Get the individual phenotype masks
-    ctrl_phenotype_mask = phenotype_matrix == 0
     case_phenotype_mask = phenotype_matrix == 1
+    ctrl_phenotype_mask = phenotype_matrix == 0
 
     # Get the individual genotype masks
     het_genotype_mask = genotype_matrix == 1
@@ -99,62 +99,60 @@ def run_phewas(
     # Now we get all the counts in two steps...
 
     # 1) Get the composite mask for all combinations
-    ctrl_het_mask = ctrl_phenotype_mask * het_genotype_mask.T
-    ctrl_ref_mask = ctrl_phenotype_mask * ref_genotype_mask.T
-    ctrl_alt_mask = ctrl_phenotype_mask * alt_genotype_mask.T
     case_het_mask = case_phenotype_mask * het_genotype_mask.T
     case_ref_mask = case_phenotype_mask * ref_genotype_mask.T
     case_alt_mask = case_phenotype_mask * alt_genotype_mask.T
+    ctrl_het_mask = ctrl_phenotype_mask * het_genotype_mask.T
+    ctrl_ref_mask = ctrl_phenotype_mask * ref_genotype_mask.T
+    ctrl_alt_mask = ctrl_phenotype_mask * alt_genotype_mask.T
 
     # 2) Get the counts for each combination
-    ctrl_het_num = ctrl_het_mask.sum(axis=0)
-    ctrl_hom_ref_num = ctrl_ref_mask.sum(axis=0)
-    ctrl_hom_alt_num = ctrl_alt_mask.sum(axis=0)
-
     case_het_num = case_het_mask.sum(axis=0)
     case_hom_ref_num = case_ref_mask.sum(axis=0)
     case_hom_alt_num = case_alt_mask.sum(axis=0)
 
+    ctrl_het_num = ctrl_het_mask.sum(axis=0)
+    ctrl_hom_ref_num = ctrl_ref_mask.sum(axis=0)
+    ctrl_hom_alt_num = ctrl_alt_mask.sum(axis=0)
     # Sum alleles here for convenience
-    ctrl_ref_num = ctrl_hom_ref_num * 2 + ctrl_het_num
-    ctrl_alt_num = ctrl_hom_alt_num * 2 + ctrl_het_num
     case_ref_num = case_hom_ref_num * 2 + case_het_num
     case_alt_num = case_hom_alt_num * 2 + case_het_num
+    ctrl_ref_num = ctrl_hom_ref_num * 2 + ctrl_het_num
+    ctrl_alt_num = ctrl_hom_alt_num * 2 + ctrl_het_num
 
     # Moving on...
 
     # Add pseudocounts prior to calculating the odds ratios
-    ctrl_ref_psu = ctrl_ref_num + 1
-    ctrl_alt_psu = ctrl_alt_num + 1
     case_ref_psu = case_ref_num + 1
     case_alt_psu = case_alt_num + 1
+    ctrl_ref_psu = ctrl_ref_num + 1
+    ctrl_alt_psu = ctrl_alt_num + 1
 
     # Calculate allele frequencies here for convenience
-    ctrl_ref_af = ctrl_ref_psu / (ctrl_ref_psu + ctrl_alt_psu)
-    ctrl_alt_af = ctrl_alt_psu / (ctrl_ref_psu + ctrl_alt_psu)
     case_ref_af = case_ref_psu / (case_ref_psu + case_alt_psu)
     case_alt_af = case_alt_psu / (case_ref_psu + case_alt_psu)
+    ctrl_ref_af = ctrl_ref_psu / (ctrl_ref_psu + ctrl_alt_psu)
+    ctrl_alt_af = ctrl_alt_psu / (ctrl_ref_psu + ctrl_alt_psu)
 
     # Sanity check...
     assert np.all(ctrl_ref_af + ctrl_alt_af == 1)
     assert np.all(case_ref_af + case_alt_af == 1)
 
     # Calculate odds ratios
+    odds_ratio = (case_alt_psu * ctrl_ref_psu) / (case_ref_psu * ctrl_alt_psu)
 
     # Sanity check...
     # odds_ratio_long = (case_alt_psu / case_ref_psu) / (ctrl_alt_psu / ctrl_ref_psu)
     # np.allclose(odds_ratio, odds_ratio_long)
 
-    odds_ratio = (case_alt_psu * ctrl_ref_psu) / (case_ref_psu * ctrl_alt_psu)
-
     # Now use vecttorized fishers exact...
     # https://stackoverflow.com/questions/34947578/how-to-vectorize-fishers-exact-test
 
     _, _, twosided = pvalue_npy(
-        case_alt_num.astype(np.uint32),
-        case_ref_num.astype(np.uint32),
-        ctrl_alt_num.astype(np.uint32),
-        ctrl_ref_num.astype(np.uint32),
+        case_alt_psu.astype(np.uint32),
+        case_ref_psu.astype(np.uint32),
+        ctrl_alt_psu.astype(np.uint32),
+        ctrl_ref_psu.astype(np.uint32),
     )
 
     results_df = pd.DataFrame(
@@ -248,25 +246,29 @@ def run_full_analysis(
 def main():
     """Debug entry point for blueprint development."""
 
-    test_variant1 = "19:15373898:C:T"
-    test_variant2 = "5:33951588_C/G"
-    test_variant3 = "9_32448929_C_T"
-
     from data_services import ServiceRegistry
 
-    registry = ServiceRegistry.from_config(Config)
+    services = ServiceRegistry.from_config(Config)
+    genotype_service = services.genotype
+    phenotype_service = services.phenotype
 
-    genotype_service = registry.genotype
-    phenotype_service = registry.phenotype
+    test_variant = "19:15373898:C:T"
+    test_variant = "5:33951588_C/G"
+    test_variant = "9_32448929_C_T"
+    test_variant = "5_151467708_T_C"
+    ancestry = "EAS"
 
-    print(f"Performing PheWAS on variant {test_variant3}")
+    print(f"Performing PheWAS on variant {test_variant}")
     results = run_phewas_or_load_from_cache(
-        test_variant3,
+        test_variant,
         genotype_service,
         phenotype_service,
-        ancestry="EUR",
+        ancestry=ancestry,
         # no_cache=True,
     )
+
+    # DEBUGGING
+    results = results[results["phecode"] == "627.4"]
 
     print(results.iloc[0].T)
 
@@ -274,7 +276,30 @@ def main():
 
     print(formatted_results[0])
 
-    exit(0)
+    # Run some random PheWAS for fun...
+    variants = genotype_service.plink_variant_ids
+    ancestries = np.array(["AFR", "EAS", "EUR", "SAS"])
+
+    done = set()
+    for _ in range(1_000_000):
+        variant = np.random.choice(variants)
+        ancestry = np.random.choice(ancestries)
+        if (variant, ancestry) in done:
+            continue
+        done.add((variant, ancestry))
+        print(f"Running PheWAS for {variant} ({ancestry})")
+        try:
+            run_phewas_or_load_from_cache(
+                variant,
+                genotype_service,
+                phenotype_service,
+                ancestry=ancestry,
+                no_cache=True,
+            )
+            print(f"Successfully ran PheWAS for {variant} ({ancestry})")
+        except Exception as e:
+            print(f"Error running PheWAS for {variant} ({ancestry}): {e}")
+            continue
 
 
 if __name__ == "__main__":
